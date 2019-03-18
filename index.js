e5c4c863929644b4d6da363270454c417ee0d12a
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-18 14:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,26 +19,30 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var newCart = [];
+  var cart = getCart();
   
   if (cart.length === 0){
     return "Your shopping cart is empty.";
   }
   else if (cart.length === 1) {
-    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   }
   
   else if (cart.length === 2) {
-    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   }
   
+  
   else {
-    var newCart = [];
-    for (var i = 0; i < cart.length - 1; i++) {
-      newCart.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `);
-    }
     
-    return `In your cart, you have ${newCart}and ${getCart()[cart.length].itemName} at $${getCart()[cart.length.itemPrice]}.`;
+    for (let i = 0; i < cart.length - 1; i++) {
+      newCart.push(`${cart[i].itemName} at $${cart[i].itemPrice}, `);
+    }
   }
+    
+    return `In your cart, you have ${newCart}and ${cart[cart.length].itemName} at $${cart[cart.length.itemPrice]}.`;
+  
   
 }
 
@@ -54,14 +58,24 @@ function total() {
   }
   
   return totalPrice;
-  
-  
 }
+
 
 function removeFromCart(item) {
   // write your code here
+ 
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  else {
+    while (cart.length > 0) {
+      cart.pop();
+    }
+    return `Your total cost is $${total()} which will be charged to the card ${cardNumber}.`;
+    
+  }
 }
